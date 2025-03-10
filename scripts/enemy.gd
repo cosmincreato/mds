be1f43355987cfbe5ab7e79db81182cc082598c8
TheFluffyBoi2 <dumitrusebastian2004@gmail.com>
2025-03-10 18:31:37 +0200

    Fix movement

--- scripts/enemy.gd
+++ scripts/enemy.gd
@@ -1,6 +1,6 @@
 extends CharacterBody2D
 
-const SPEED = 100
+@export var speed = 100
 var parent : Node2D
 
 func _ready() -> void:
@@ -8,6 +8,6 @@ func _ready() -> void:
 
 func _physics_process(delta: float) -> void:
 	if parent:
-		var direction = position.direction_to(parent.seeking.position)
-		velocity = direction * SPEED
+		var direction = global_position.direction_to(parent.seeking.position)
+		velocity = direction * speed
 		move_and_slide()
