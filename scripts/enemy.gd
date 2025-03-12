b1527c093c168a2188bac9246b411eb184757e87
Mărășoiu Cosmin <marasoiu.cos@gmail.com>
2025-03-12 20:27:13 +0200

    healthbar

--- scripts/enemy.gd
+++ scripts/enemy.gd
@@ -1,20 +1,19 @@
 extends Node2D
 
 # Nodul catre care se va deplasa inamicul
-var seeking : Node2D = null
+@export var seeking : Node2D = null
 var speed : int = 300
 
 func _ready() -> void:
-	
-	var healthBar : Node = $HealthBar
-	
 	# Dependency Injection pentru copiii care au nevoie de variabile din parinte
 	for child in get_children():
 		if child.has_method("set_dependencies"):
 			child.set_dependencies(self)
-			
-	healthBar.health_changed.connect(_on_health_changed)
-	
-func _on_health_changed(health : int) -> void:
-	if health <= 0:
-		queue_free()
+
+
+func _on_health_bar_damage_taken(_new_health: int, amount: int) -> void:
+	print(name + " has taken " + str(amount) + " damage.")
+
+
+func _on_health_bar_died() -> void:
+	queue_free()
--- scripts/health_component.gd
+++ scripts/health_component.gd
@@ -1,14 +1,34 @@
-extends Node
+extends Control
 
-signal health_changed(health)
+signal damage_taken(new_health : int, amount : int)
+signal healed(new_health : int, amount : int)
+signal died()
 
 @export var max_health: int = 1000
 var health: int
 
 func _ready() -> void:
 	health = max_health
+	
+	# Dependency Injection pentru copiii care au nevoie de variabile din parinte
+	for child in get_children():
+		if child.has_method("set_dependencies"):
+			child.set_dependencies(self)
 
 # Poate functiona drept damage sau heal
-func set_health(amount: int) -> void:
-	health = amount
-	emit_signal("health_changed", health)
+func take_damage(amount : int):
+	health -= amount
+	
+	if health <= 0:
+		health = 0
+		emit_signal("died")
+	
+	emit_signal("damage_taken", health, amount)
+		
+func heal(amount : int):
+	health += amount
+	
+	if health > max_health:
+		health = max_health
+	
+	emit_signal("healed", health, amount)
