0cc3b75d5b4e0372ad4a1a1c3a641a6a1bf6a596
Mărășoiu Cosmin <marasoiu.cos@gmail.com>
2025-05-05 20:00:15 +0300

    update dupa 1000 ani

--- scripts/entities/enemies/skeleton.gd
+++ scripts/entities/enemies/skeleton.gd
@@ -2,11 +2,5 @@ class_name Skeleton extends Enemy
 
 func _ready() -> void:
 	# Apelam metoda din superclasa
-	super()
+	super._ready()
 	print("Skeleton spawned")
-
-func _on_health_component_damage_taken(new_health: int, _amount: int) -> void:
-	super(new_health, _amount)
-
-func _on_health_component_died() -> void:
-	super()
