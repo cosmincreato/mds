8f14627b1f8a11cc790f533cbf7d729e28893b04
horica-svg <horiazafiu2@gmail.com>
2025-05-06 11:28:41 +0300

    varianta initiala cu Tower

--- scripts/main_scene.gd
+++ scripts/main_scene.gd
@@ -10,39 +10,94 @@ var allies_dir = DirAccess.open("res://scenes/entities/allies/")
 var allies_dictionary: Dictionary = {}
 var hovering: bool = false
 
+var current_ally_type = 1
+
 func _ready() -> void:
-	# Cream un dictionar in care retinem toate locatiile din fisiere
-	# a scenelor pentru aliati
+	print("Director analizat: " + allies_dir.get_current_dir())
+
 	var index = 1
-	var path: String
-	for ally in allies_dir.get_files():
-		path = allies_dir.get_current_dir() + "/" + str(ally)
-		allies_dictionary[index] = load(path)
-		index += 1
+	allies_dir.list_dir_begin()
+	var file_name := allies_dir.get_next()
+
+	while file_name != "":
+		if file_name.ends_with(".tscn"):
+			var path = allies_dir.get_current_dir() + "/" + file_name
+			var scene = load(path)
+			
+			if scene:
+				print("Index $index:")
+				print(" - Fisier: $file_name")
+				print(" - Cale: $path")
+				print(" - Scena hash: " + str(scene.get_instance_id()))
+				
+				allies_dictionary[index] = scene
+				print("Loaded ally type " + str(index) + ": " + file_name)
+				index += 1
+			else:
+				print("EROARE: Nu s-a putut încărca scena: " + path)
+		file_name = allies_dir.get_next()
+	allies_dir.list_dir_end()
 
 	base.find_child("Hurtbox").mouse_entered.connect(_on_mouse_entered)
 	base.find_child("Hurtbox").mouse_exited.connect(_on_mouse_exited)
-	
-	
+
 func _input(event: InputEvent) -> void:
-	#TODO verificare pentru fiecare tip de aliat nu doar pentru cel cu cheia 1
-	if event.is_action_pressed("1") and !ally_spawn_canvas.visible:
-		var ally_scene = allies_dictionary[1]
+	if event.is_action_pressed("left_mouse") and ally_spawn_canvas.visible:
+		_on_canvas_ally_spawned(ally_spawn_canvas.mouse_x, ally_spawn_canvas.mouse_y, current_ally_type)
+		return
+	
+	if event.is_action_pressed("1"):
+		current_ally_type = 1
+	elif event.is_action_pressed("2"):
+		current_ally_type = 2
+	else:
+		return
+	
+	print("current_ally_type:", current_ally_type, " | Tip:", typeof(current_ally_type))
+	
+	if !ally_spawn_canvas.visible:
+		var ally_scene = allies_dictionary[current_ally_type]
 		var ally = ally_scene.instantiate()
 		var sprite = ally.get_node_or_null("CharacterBody2D/Sprite2D")
 		var cost_component = ally.get_node_or_null("CostComponent")
-		ally_spawn_canvas.add_canvas_items(sprite.texture, cost_component.cost)
+		if sprite and cost_component:
+			ally_spawn_canvas.add_canvas_items(sprite.texture, cost_component.cost)
 		ally.queue_free()
-	elif event.is_action_pressed("1") and ally_spawn_canvas.visible:
+	else:
 		ally_spawn_canvas.remove_canvas_items()
 
 # Cand AllySpawnCanvas semnaleaza ca incercam sa spawnam o unitate
 func _on_canvas_ally_spawned(mouse_x: float, mouse_y: float, type: int) -> void:
 	if !hovering:
+		if !allies_dictionary.has(type):
+			print("EROARE: Nu există ally de tipul:", type)
+			return
+
 		var ally = allies_dictionary[type].instantiate()
 		ally.position = Vector2(mouse_x, mouse_y)
-		ally.find_child("Hurtbox").mouse_entered.connect(_on_mouse_entered)
-		ally.find_child("Hurtbox").mouse_exited.connect(_on_mouse_exited)
+		
+		# Debug inițial
+		print("Spawnez entitate de tip: " + str(type))
+		print(" - Nod root inițial: " + ally.name)
+		
+		add_child(ally)
+		
+		#Debug dupa adaugare:
+		print(" - Nod root după adăugare: " + ally.name)
+		# Debug: verificăm ce tip de nod avem
+		#print("Spawnez entitate de tip: " + str(type))
+		#print(" - Nod root: " + ally.name)
+		#var sprite = ally.get_node_or_null("CharacterBody2D/Sprite2D")
+		#if sprite and sprite.texture:
+			#print(" - Textura: " + sprite.texture.get_path())
+		#else:
+			#print(" - Textura: lipsă sau invalidă")
+		# Conectăm evenimentele dacă au un nod "Hurtbox"
+		var hurtbox = ally.get_node_or_null("Hurtbox")
+		if hurtbox:
+			hurtbox.mouse_entered.connect(_on_mouse_entered)
+			hurtbox.mouse_exited.connect(_on_mouse_exited)
+		# Adăugăm efectiv pe scenă
 		buy_ally(ally)
 	else:
 		print("Invalid position")
@@ -58,9 +113,15 @@ func buy_ally(ally : Node2D) -> void:
 		var cost_component = ally.get_node_or_null("CostComponent")
 		if cost_component:
 			GameManager.spend_gold(cost_component.cost)
-			add_child(ally)
+
+		# Debug: Verificăm numele nodului rădăcină
+		print("Nume nod root în buy_ally: " + ally.name)
+
+		add_child(ally)
 	else:
 		print("Not enough gold")
+		ally.queue_free()
+
 	ally_spawn_canvas.remove_canvas_items()
 	
 # Cand EnemySpawnManager semnaleaza ca un inamic s-a spawnat
